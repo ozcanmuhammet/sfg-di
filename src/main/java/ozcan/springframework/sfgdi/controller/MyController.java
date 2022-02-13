47d889819f49c83a06d52c89bfdad3d896e65931
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-02-14 00:59:57 +0300

    Using @Primary Annotation

--- src/main/java/ozcan/springframework/sfgdi/controller/MyController.java
+++ src/main/java/ozcan/springframework/sfgdi/controller/MyController.java
@@ -1,9 +1,22 @@
 package ozcan.springframework.sfgdi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ozcan.springframework.sfgdi.service.GreetingService;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+
 
     public String sayHello(){
         System.out.println("Hello World!");
