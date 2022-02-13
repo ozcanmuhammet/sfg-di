00378de73a4a13080f3ffedb4fe6e52d30fbfe49
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-02-14 01:23:06 +0300

    Using @Profile Annotation

---
+++ src/main/java/ozcan/springframework/sfgdi/service/I18nEnglishGreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package ozcan.springframework.sfgdi.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18nService")
+public class I18nEnglishGreetingServiceImpl implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ src/main/java/ozcan/springframework/sfgdi/service/I18nSpanishGreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package ozcan.springframework.sfgdi.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18nService")
+public class I18nSpanishGreetingServiceImpl implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
