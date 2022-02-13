d53e941d16661cf3a9d811e8f2061537b2b29f1a
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-02-14 01:35:05 +0300

    Using Default Profile

--- src/main/java/ozcan/springframework/sfgdi/service/I18nSpanishGreetingServiceImpl.java
+++ src/main/java/ozcan/springframework/sfgdi/service/I18nSpanishGreetingServiceImpl.java
@@ -3,7 +3,7 @@ package ozcan.springframework.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingServiceImpl implements GreetingService{
 
